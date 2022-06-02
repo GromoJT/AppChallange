71b80c9209c60ccc354b2c473ca7ab302df214a7
GromoJT <85227567+GromoJT@users.noreply.github.com>
2022-06-02 10:48:19 +0200

    Działające fragmenty

--- app/src/main/java/com/example/challanger/MainActivity.kt
+++ app/src/main/java/com/example/challanger/MainActivity.kt
@@ -7,18 +7,20 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.graphics.toColor
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.title ="TESTER"
+        //supportActionBar?.title ="TESTER"
 
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout = findViewById(R.id.drawerLayout)
         val navView: NavigationView = findViewById(R.id.nav_view)
 
         toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
@@ -28,11 +30,15 @@ class MainActivity : AppCompatActivity() {
         toggle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        replaceFragment(HomeFragment(),"Home")
 
         navView.setNavigationItemSelectedListener {
+
+            it.isCheckable=true
+
             when(it.itemId){
-                R.id.nav_home -> Toast.makeText(applicationContext,"Kliknięto dom",Toast.LENGTH_SHORT).show()
-                R.id.nav_menu_1 -> Toast.makeText(applicationContext,"Kliknięto 1",Toast.LENGTH_SHORT).show()
+                R.id.nav_home -> replaceFragment(HomeFragment(),it.title.toString())
+                R.id.nav_menu_1 -> replaceFragment(FirstFragment(),it.title.toString())
                 R.id.nav_menu_2 -> Toast.makeText(applicationContext,"Kliknięto 2",Toast.LENGTH_SHORT).show()
                 R.id.nav_menu_3 -> Toast.makeText(applicationContext,"Kliknięto 3",Toast.LENGTH_SHORT).show()
                 R.id.nav_menu_5 -> Toast.makeText(applicationContext,"Kliknięto 5",Toast.LENGTH_SHORT).show()
@@ -40,6 +46,16 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment, title:String){
+        val fragmentMenager = supportFragmentManager
+        val fragmentTransaction = fragmentMenager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
 
     }
 
